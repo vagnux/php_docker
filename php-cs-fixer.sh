ac1472fe0b46e9180691001c3a94bfb6e6d0e34b
Vagner Rodrigues <vagner.rodrigues@tradeupgroup.com>
2019-11-12 10:51:45 -0300

    add ctrl+c support

--- php-cs-fixer.sh
+++ php-cs-fixer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 namePath=`shuf -i100-999 -n1`
-docker run -i --rm --name my-running-script"$namePath" --network="host"  -v "$PWD":"$PWD" -v /home:/home -v /tmp:/tmp -u $(id -u)  -w "$PWD" php_docker /usr/bin/php-cs-fixer "$@"
+docker run -t -i --rm --name my-running-script"$namePath" --network="host" -v /dev:/dev -v /tmp:/tmp -v /tmp:/.config  -v "$PWD":"$PWD" -v /home:/home -u $(id -u)  -w "$PWD" php_docker /usr/bin/php-cs-fixer "$@"  
 
