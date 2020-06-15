c201d5ce4ae397eb5194d81a4724df1b742e50bd
Vagner Rodrigues <vagnux@gmail.com>
2020-06-15 17:59:01 -0300

    php old fix removed

--- php-cs-fixer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-namePath=`shuf -i100-999 -n1`
-param="$@";
-if [ $# -lt 1 ]; then
-   read param
-fi
-docker run  -i --name my-running-script"$namePath" --network="host" -v /dev:/dev -v /tmp:/tmp -v /tmp:/.config  -v "$PWD":"$PWD" -v /home:/home -u $(id -u)  -w "$PWD" php_docker /usr/bin/php-cs-fixer "$param"  
-
