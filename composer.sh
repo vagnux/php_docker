78847891ab043245aed2ea0fb47c8872a171df9b
Vagner Rodrigues <vagnux@gmail.com>
2021-07-02 13:53:52 -0300

    fix php.sh

--- composer.sh
+++ composer.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 namePath=`shuf -i100-999 -n1`
-docker run -t -i --rm --name my-running-script"$namePath" --network="host"  -v "$PWD":"$PWD" -v /home:/home  -v /tmp:/.composer -u $(id -u ${USER}):$(id -g ${USER}) -w "$PWD" php_docker /usr/bin/composer "$@"
-
+docker run -t -i --rm --name my-running-script"$namePath" --network="host"  -v "$PWD":"$PWD" -v /home:/home  -v /tmp:/.composer -u $(id -u ${USER}):$(id -g ${USER}) -w "$PWD" php_docker /usr/local/bin/composer "$@"
--- pkginstall.sh
+++ pkginstall.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
+apt update
+DEBIAN_FRONTEND=noninteractive apt -y install php-cli \
+php-mbstring \
+php-mysqli \
+php-pdo \
+freetds-common \
+freetds-bin \
+unixodbc \
+php-sybase \
+php-memcache \
+php-memcached \
+php-curl \
+php-zip \
+php-gd \
+php-xml \
+php-mbstring \
+php-gmp \
+php-soap \
+php-xdebug \
+wget \
+curl \
+php-sybase \
+php-sqlite3 \
+default-mysql-client
+echo "Composer installing"
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer 
 
-apt  update
-DEBIAN_FRONTEND=noninteractive apt -y install php-cli
-#DEBIAN_FRONTEND=noninteractive apt -y install apache2
-DEBIAN_FRONTEND=noninteractive apt -y install php-mbstring
-DEBIAN_FRONTEND=noninteractive apt -y install php-mysqli
-DEBIAN_FRONTEND=noninteractive apt -y install php-pdo
-#DEBIAN_FRONTEND=noninteractive apt -y install libapache2-mod-php
-DEBIAN_FRONTEND=noninteractiveapt -y install freetds-common freetds-bin unixodbc php-sybase
-DEBIAN_FRONTEND=noninteractive apt -y install php-memcache
-DEBIAN_FRONTEND=noninteractive apt -y install php-memcached
-DEBIAN_FRONTEND=noninteractive apt -y install php-curl
-DEBIAN_FRONTEND=noninteractive apt -y install php-zip
-DEBIAN_FRONTEND=noninteractive apt -y install php-gd
-DEBIAN_FRONTEND=noninteractive apt -y install php-xml
-DEBIAN_FRONTEND=noninteractive apt -y install php-mbstring
-DEBIAN_FRONTEND=noninteractive apt -y install php-gmp
-DEBIAN_FRONTEND=noninteractive apt -y install composer
-DEBIAN_FRONTEND=noninteractive apt -y install php-soap
-DEBIAN_FRONTEND=noninteractive apt -y install php-xdebug
-DEBIAN_FRONTEND=noninteractive apt -y install wget
-DEBIAN_FRONTEND=noninteractive apt -y install php-sybase
-DEBIAN_FRONTEND=noninteractive apt -y install php-sqlite3
-#DEBIAN_FRONTEND=noninteractive apt -y install mycli
-DEBIAN_FRONTEND=noninteractive apt -y install default-mysql-client
+echo "phpcs installing"
 wget  https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar -O /usr/bin/phpcs
 chmod +x /usr/bin/phpcs
 #find /etc/ -iname php.ini | awk  '{print "cat /xdebug.ini >> "$1}' | /bin/bash
